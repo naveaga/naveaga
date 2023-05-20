78c21c3c717f7c483df415424d2286f433205a0b
naveaga <109790015+naveaga@users.noreply.github.com>
2023-05-20 16:15:43 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+#hi everyone im here to show you how to make your first program
+#to  start make sure u download python and add to right path
+#after that we can start
+#follow me
+
+#copy the (7) line
+num = int(input("Enter your number bettween 1-10: ")) #the (num) is varible and (int) its for number and (input) u know what is input and after that u have something like (print) ya this is for to enter what u want to write in the output
+
+while not num == "q": #and (while not etc.) this for stop the loop if its like mine u write (while not num == "q":) and after the prints and use to print num in the function print its gonna to be in the seacond print and u write.. print(f"etc. {etc}") 
+    print(f"your number is {num}") #i tell you rn
+    num = input("enter your number again (q to leave): ")# like the first line but now we dont use int before input cuz its gonna loop the answer in the first line
+
+print("bye")#this print is for (q to leave) only if u answer "q" its gonna print bye
